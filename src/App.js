21f78df01e3bc86507746bf17d5220b3a5046982
AlanAlv <37889366+AlanAlv@users.noreply.github.com>
2019-11-25 22:22:15 -0600

    Stateful components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+class HelloWorld extends React.Component {
+
+  state = {
+    show: true
+  }
+
+  toggleShow = () => {
+    this.setState({show: !this.state.show})
+  }
+
+  render(){
+    if (this.state.show){
+      return (
+        <div id="hello">
+        <h3>{this.props.subtitle}</h3>
+        {this.props.myText}
+        <button onClick={this.toggleShow   }>Toggle Show</button>
+      </div>
+      );
+    }
+    else{
+      return ( 
+      <h1>There are not elements
+      <button onClick={this.toggleShow }> Toggle Show</button>
+
+      </h1>
+
+      );
+    }
+  }
+}
+
+/*
+function HelloWorld(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="hello">
+      <h3>{props.subtitle}</h3>
+      {props.myText}
+    </div>
+  );
+}
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        This is my component <HelloWorld/>
+      </div>
+    )
+  }
+}
+
+const App = () => 
+  <div>
+    This is my component <HelloWorld/>
+  </div>
+*/
+function App(props) {
+  return (
+    <div>
+      This is my component: 
+      <HelloWorld myText="Hello Alan" subtitle="Loremp ipsum"/> 
+      <HelloWorld myText="Hola mundo"  subtitle="Qwerty"/>
+      <HelloWorld myText="Hola"  subtitle="Asdf"/>
     </div>
   );
 }
