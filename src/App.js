a36cd67d1c09bfbea3e77ead3d40b84d83abe982
AlanAlv <37889366+AlanAlv@users.noreply.github.com>
2019-11-25 23:10:24 -0600

    Tasks component

--- src/App.js
+++ src/App.js
@@ -1,73 +1,22 @@
 import React from 'react';
 import './App.css';
+import tasks from './sample/tasks.json'
+import Tasks from './components/Tasks'
 
-
-class HelloWorld extends React.Component {
-
-  state = {
-    show: true
-  }
-
-  toggleShow = () => {
-    this.setState({show: !this.state.show})
-  }
-
-  render(){
-    if (this.state.show){
-      return (
-        <div id="hello">
-        <h3>{this.props.subtitle}</h3>
-        {this.props.myText}
-        <button onClick={this.toggleShow   }>Toggle Show</button>
-      </div>
-      );
-    }
-    else{
-      return ( 
-      <h1>There are not elements
-      <button onClick={this.toggleShow }> Toggle Show</button>
-
-      </h1>
-
-      );
-    }
-  }
-}
-
-/*
-function HelloWorld(props) {
-  return (
-    <div id="hello">
-      <h3>{props.subtitle}</h3>
-      {props.myText}
-    </div>
-  );
-}
+console.log(tasks);
 
 class App extends React.Component {
-  render(){
-    return (
-      <div>
-        This is my component <HelloWorld/>
-      </div>
-    )
+  
+  state = {
+    tasks: tasks
   }
-}
-
-const App = () => 
-  <div>
-    This is my component <HelloWorld/>
-  </div>
-*/
-function App(props) {
-  return (
-    <div>
-      This is my component: 
-      <HelloWorld myText="Hello Alan" subtitle="Loremp ipsum"/> 
-      <HelloWorld myText="Hola mundo"  subtitle="Qwerty"/>
-      <HelloWorld myText="Hola"  subtitle="Asdf"/>
-    </div>
-  );
+  render(){
+    return(
+      <div>
+        <Tasks tasks={this.state.tasks} />
+      </div>
+    );
+  }
 }
 
 export default App;
