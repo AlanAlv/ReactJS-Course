9946e5ad0dfd63060db4107a74b3fe18077861dd
AlanAlv <37889366+AlanAlv@users.noreply.github.com>
2019-11-27 00:29:23 -0600

    Add new task

--- src/App.js
+++ src/App.js
@@ -6,17 +6,28 @@ import tasks from './sample/tasks.json'
 import Tasks from './components/Tasks'
 import TaskForm from './components/TaskForm'
 
-console.log(tasks);
 
 class App extends React.Component {
   
   state = {
     tasks: tasks
   }
+
+  addTask = (title, description) => {
+    const newTask = {
+      title: title,
+      description: description,
+      id: this.state.tasks.length
+    }
+    this.setState({
+      tasks: [...this.state.tasks, newTask]
+    })
+  }
+
   render(){
     return(
       <div>
-        <TaskForm />
+        <TaskForm addTask={this.addTask}/>
         <Tasks tasks={this.state.tasks} />
       </div>
     );
