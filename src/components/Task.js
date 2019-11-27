1ed8f7786590fa1eda8fb5bd856c322d9d795ab8
AlanAlv <37889366+AlanAlv@users.noreply.github.com>
2019-11-26 19:46:32 -0600

    Styles

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,16 +1,27 @@
 import React from 'react'
 
+
 class Task extends React.Component {
+
+    styleCompleted () {
+        return {
+            fontSize: '20px',
+            color: this.props.task.done ? 'gray' :'black',
+            textDecoration: this.props.task.done ? 'line-through' : 'none'
+        }
+    }
+
     render() {
         const{task} = this.props;
+
         return (
-            <div>
+            <div style={this.styleCompleted()}>
                 {task.title} - 
                 {task.description} - 
                 {task.done} - 
                 {task.id}
                 <input type="checkbox" />
-                <button>
+                <button style={btnDelete}>
                     X
                 </button>
             </div>
@@ -18,4 +29,14 @@ class Task extends React.Component {
     }
 }
 
+const btnDelete = {
+    fontSize: '18px',
+    background: '#ea2027',
+    color: '#fff',
+    border: 'none',
+    padding: '10px 15px',
+    borderRadius: '50%',
+    cursor: 'pointer'
+
+}
 export default Task;
